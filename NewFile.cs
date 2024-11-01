0471b6ccf6cd237e00f8ae7c0221be6af778617b
Lesia <lesiakirat@gmail.com>
2024-11-01 10:24:01 +0200

    Changes add

---
+++ NewFile.cs
@@ -0,0 +1,30 @@
+using System;
+
+// Параметр з ключовим словом params повинен бути тільки один і стояти останнім у списку параметрів методу.
+
+namespace Arrays
+{
+	class Program
+	{
+		static void ShowArray(string name, params int[] array)
+		{
+			Console.Write(name);
+			int s = 1;
+
+			for (int i = 0; i < array.Length; i++)
+			{
+				s = s + i;
+				//s = s + array[i];
+				Console.Write($"{s} ");
+			}
+		}
+
+		static void Main()
+		{
+			ShowArray("Numbers: ", 0, 1, 2, 3, 4, 5, 6, 7, 8, 9);
+
+			// Delay.
+			Console.ReadKey();
+		}
+	}
+}
